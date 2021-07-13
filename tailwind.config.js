db928df4929288b568e6e6d6f9aabb0edfbd0df2
GAMHnile <georgeamhuweh@gmail.com>
2021-07-13 21:54:15 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    debugScreens: {
+      position: ["top", "left"],
+    },
+    extend: {
+      fontFamily: {
+        headline: ['Oswald']
+      },
+      colors: {
+        mainColor: "#212f49"
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [require("tailwindcss-debug-screens")],
+};
